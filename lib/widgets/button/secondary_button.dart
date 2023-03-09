507ef536218f565a9a995be124c8acdde102cf80
onishitaisei <taisei.developer@gmail.com>
2023-03-09 18:47:05 +0900

    secondary_buttonを作成

---
+++ lib/widgets/button/secondary_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:theme_extension_sample/widgets/button/primary_button_style.dart';
+
+class SecondaryButton extends StatelessWidget {
+  const SecondaryButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+    this.style,
+  });
+
+  final String text;
+  final VoidCallback onPressed;
+  final PrimaryButtonStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    final defaultStyle = Theme.of(context).extension<PrimaryButtonStyle>()!;
+    final backgroundColor =
+        style?.backgroundColor ?? defaultStyle.backgroundColor;
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
+      ),
+      onPressed: onPressed,
+      child: Text(text),
+    );
+  }
+}
